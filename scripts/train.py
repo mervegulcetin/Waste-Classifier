97cdc8d404e239c45a4f219c594131fd7bf952a6
Mervegül Çetin <mervecetin0908@gmail.com>
2025-04-10 23:31:29 +0300

    fine-tuning for top layers

--- scripts/train.py
+++ scripts/train.py
@@ -36,7 +36,27 @@ val_gen = datagen.flow_from_directory(
 
 # creating the model
 base_model = MobileNetV2(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+base_model.trainable = False 
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 predictions = Dense(train_gen.num_classes, activation='softmax')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
+
+model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+
+#training the model
+history = model.fit(train_gen, validation_data=val_gen, epochs=EPOCHS)
+
+
+# Fine Tuning
+base_model.trainable = True  # Unfreezing the base model
+
+# Fine-tuning the top layers
+for layer in base_model.layers[:-20]:  # Unfreezing the last 20 layers
+    layer.trainable = False
+
+# Compile the model with a lower learning rate
+model.compile(optimizer=Adam(learning_rate=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Training the model
+history = model.fit(train_gen, validation_data=val_gen, epochs=EPOCHS)
